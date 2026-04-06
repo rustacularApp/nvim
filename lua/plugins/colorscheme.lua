0014d9b729cc1646ded49b0e27bdd4147cf2569f
h4ck4l1 <sohailmd123@gmail.com>
2026-04-06 11:06:40 +0530

    add
    add

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,8 @@ return {
 		"olimorris/onedarkpro.nvim",
 		priority = 1000,
 		config = function()
-			require("onedarkpro").setup({
-				options = {
-					transparency = true,
-				},
-			})
-			vim.cmd("colorscheme onedark_dark")
+			require("onedarkpro").setup({})
+			vim.cmd("colorscheme onedark_vivid")
 		end,
 	},
 }
