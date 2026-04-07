794b2ffd0755a7079a756758d4a371bfa3538ef4
h4ck4l1 <sohailmd123@gmail.com>
2026-04-08 01:32:18 +0530

    add
    add

--- lua/plugins/dart-treesitter.lua
+++ lua/plugins/dart-treesitter.lua
@@ -1,10 +1,37 @@
 return {
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = function(_, opts)
+	-- 		opts.indent = opts.indent or {}
+	-- 		opts.auto_install = true
+	-- 		opts.indent.disable = opts.indent.disable or {}
+	-- 		table.insert(opts.indent.disable, "dart")
+	-- 	end,
+	-- },
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.indent = opts.indent or {}
-			opts.indent.disable = opts.indent.disable or {}
-			table.insert(opts.indent.disable, "dart")
-		end,
-	},
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs",
+		opts = {
+			ensure_installed = {
+				"lua",
+				"dart",
+				"rust",
+				"typescript",
+				"tsx",
+				"javascript",
+				"jsx"
+			},
+			auto_install = true,
+			highlight = {
+				enable = true
+			},
+			indent = {
+				enable = true,
+				disable = {
+					"dart"
+				}
+			}
+		}
+	}
 }
