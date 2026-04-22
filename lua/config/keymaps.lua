05654929b8781cf0770ad8bd887d78252f4d8218
h4ck4l1 <sohailmd123@gmail.com>
2026-04-23 02:09:30 +0530

    add
    add

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -443,3 +443,12 @@ end, { desc = "Jump to prev indent column" })
 
 vim.keymap.set("n","<leader>e", "<leader>fE", { desc = "Explorer Snacks (cwd)", remap = true})
 vim.keymap.set("n","<leader>E", "<leader>fe", { desc = "Explorer Snacks (rwd)", remap = true})
+
+vim.keymap.set("n", "<leader>tr", function()
+  for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
+    client:stop()
+  end
+
+  vim.cmd("edit")
+  vim.notify("LSP restarted for Typst")
+end, { desc = "Typst/LSP Restart" })
