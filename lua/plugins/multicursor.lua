1ab2329a1bc74f51dcdf113ce8de3b9636f37460
h4ck4l1 <sohailmd123@gmail.com>
2026-06-07 07:52:12 +0530

    add
    add

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,66 +1,5 @@
 return {
 	{
-		"jake-stewart/multicursor.nvim",
-		branch = "1.0",
-		config = function()
-			local mc = require("multicursor-nvim")
-			mc.setup()
-
-			local set = vim.keymap.set
-
-			-- Add or skip cursor above/below the main cursor.
-			set({"n", "x"}, "<leader>h<down>", function() mc.lineAddCursor(-1) end)
-			set({"n", "x"}, "<leader>h<down>", function() mc.lineAddCursor(1) end)
-			set({"n", "x"}, "<leader>hr<up>", function() mc.lineSkipCursor(-1) end)
-			set({"n", "x"}, "<leader>hr<down>", function() mc.lineSkipCursor(1) end)
-
-			-- Add or skip adding a new cursor by matching word/selection
-			set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
-			set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-			set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-			set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
-
-			-- Add and remove cursors with control + left click.
-			set("n", "<c-leftmouse>", mc.handleMouse)
-			set("n", "<c-leftdrag>", mc.handleMouseDrag)
-			set("n", "<c-leftrelease>", mc.handleMouseRelease)
-
-			-- Disable and enable cursors.
-			set({"n", "x"}, "<c-q>", mc.toggleCursor)
-
-			-- Mappings defined in a keymap layer only apply when there are
-			-- multiple cursors. This lets you have overlapping mappings.
-			mc.addKeymapLayer(function(layerSet)
-
-				-- Select a different cursor as the main one.
-				layerSet({"n", "x"}, "<left>", mc.prevCursor)
-				layerSet({"n", "x"}, "<right>", mc.nextCursor)
-
-				-- Delete the main cursor.
-				layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
-
-				-- Enable and clear cursors using escape.
-				layerSet("n", "<esc>", function()
-					if not mc.cursorsEnabled() then
-						mc.enableCursors()
-					else
-						mc.clearCursors()
-					end
-				end)
-			end)
-
-			-- Customize how cursors look.
-			local hl = vim.api.nvim_set_hl
-			hl(0, "MultiCursorCursor", { reverse = true })
-			hl(0, "MultiCursorVisual", { link = "Visual" })
-			hl(0, "MultiCursorSign", { link = "SignColumn"})
-			hl(0, "MultiCursorMatchPreview", { link = "Search" })
-			hl(0, "MultiCursorDisabledCursor", { reverse = true })
-			hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-			hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
-		end
-	},
-	{
 		"brenton-leighton/multiple-cursors.nvim",
 		version = "0.15",  -- Use the latest tagged version
 		opts = {},  -- This causes the plugin setup function to be called
@@ -72,17 +11,17 @@ return {
 			{"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move down"},
 
 			{"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove cursor on mouse click"},
-			{"<C-Return>", "<Cmd>MultipleCursorsAddDelete<CR>", mode = {"n"}, desc = "Add a locked cursor or remove an existing cursor"},
+			{"<leader>hmr", "<Cmd>MultipleCursorsAddDelete<CR>", mode = {"n"}, desc = "Add a locked cursor or remove an existing cursor"},
 
-			{"<Leader>m", "<Cmd>MultipleCursorsAddVisualArea<CR>", mode = {"x"}, desc = "Add cursors to the lines of the visual area"},
+			{"<Leader>hm", "<Cmd>MultipleCursorsAddVisualArea<CR>", mode = {"x"}, desc = "Add cursors to the lines of the visual area"},
 
-			{"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
-			{"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
+			{"<Leader>ha", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
+			{"<Leader>hA", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
 
-			{"<Leader>d", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
-			{"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
+			{"<Leader>hd", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
+			{"<Leader>hD", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
 
-			{"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
+			{"<Leader>hl", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
 		},
 	},
 }
